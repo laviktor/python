fcd88f6cf53312eb1f8977c6fb7e8dd78e327056
Kato La <56576057+laviktor@users.noreply.github.com>
2024-05-21 02:55:04 -0400

    Update challenge_03.py

--- task_03/challenge_03.py
+++ task_03/challenge_03.py
@@ -2,4 +2,11 @@ x1 = 1
 x2 = 2
 if x1 < x2:
   print(str(x1) + ', ' + str(x2)
-        else: print(str(x2) + ', ' + str(x1)
+else:
+  print(str(x2) + ', ' + str(x1)
+
+# Suppose there were three numbers
+x3 = 3
+if x3 < first_position:
+  print(str(x3) + ', ' + str(x2) + ', ' + str(first_position))
+else:
